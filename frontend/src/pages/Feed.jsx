3799c89f35fffa093b9d04887244bbe4a347bd2d
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-02-18 16:15:17 +0500

    new code

--- frontend/src/pages/Feed.jsx
+++ frontend/src/pages/Feed.jsx
@@ -2,17 +2,26 @@ import React, { useState } from "react";
 
 const Feed = () => {
   const [posts, setPosts] = useState([
-    { id: 1, image: "first post", caption: "this is content" },
+    { id: 1, image: "first post", caption: "this is content  __________________________________" },
 
   ]);
 
   return <>
-        
+            <h1>welcome</h1>
            <section className="feed-section">
             {
-               
+               posts.length > 0 ? (
+                posts.map((one) => (
+                  <div key={one.id} className="post-card">
+                    <h1>{one.caption}</h1>
+                  </div>
+                ))
+               ) : (
+                <p>no posts available</p>
+               )
             }
            </section>
+           
          </>;
 };
 
