743f04ea8ce21e44d1f1745276125b7f1fc58bf1
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-02-18 15:20:48 +0500

    new code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Createpost from "./pages/createpost";
+import Feed from "./pages/feed";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<div>Home</div>} />
         <Route path="/createpost" element={<Createpost />} />
+        <Route path="/feed" element={<Feed />} />
       </Routes>
     </Router>
   )
---
+++ frontend/src/pages/Feed.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+const Feed = () => {
+  const [posts, setPosts] = useState([
+    { id: 1, image: "first post", caption: "this is content" },
+
+  ]);
+
+  return <>
+        
+           <section className="feed-section">
+            {
+               
+            }
+           </section>
+         </>;
+};
+
+export default Feed;
