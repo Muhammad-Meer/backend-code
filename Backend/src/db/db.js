65b9851b4b622d326adc76081dba07a84a434dcd
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-03-04 14:24:12 +0500

    fix

--- Backend/src/db/db.js
+++ Backend/src/db/db.js
@@ -4,8 +4,13 @@ const mongosse = require('mongoose')
 
 const conectDB = () => {
   mongosse.connect(process.env.MONGO_URI)
-  .then(console.log("conected"))
-  .catch(console.log("some eroor"))
+  .then(() => {
+    console.log("successfully connect")
+  })
+
+  .catch((error) => {
+    console.log(error)
+  })
 }
 
 
