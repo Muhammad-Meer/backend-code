549bda3e9f31790bfe0cb7e164fd5cfb2bf95a8d
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-03-04 13:33:35 +0500

    new codee

--- Backend/src/user.model.js/user.model.js
+++ Backend/src/user.model.js/user.model.js
@@ -3,5 +3,23 @@ const mongoose = require('mongoose')
 
 
 const userSchema = new mongoose.Schema({
+
+  fullName: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
   
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
 })
