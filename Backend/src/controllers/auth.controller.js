dc9c0e5cd9ba62264f4f841e87d230856c0e16b2
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-03-04 16:54:13 +0500

    new code

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,41 +1,47 @@
-const  usermodel = require('../models/user.model')
- const bcrypt = require('bcrypt')
- const jwt = require('jsonwebtoken')
- const cookie = require('cookie-parser')
+const usermodel = require("../models/user.model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cookie = require("cookie-parser");
 
+async function userregister(req, res) {
+  const { fullName, email, password } = req.body;
 
+  if (!fullName || !email || !password) {
+    return res.status(400).json({
+      message: "all fields is required",
+    });
+  }
 
-  async function userregister(req , res) {
-      const { fullName, email, password } = req.body;
+  const emailisalreadyexist = await usermodel.findOne({
+    email,
+  });
 
+  if (emailisalreadyexist) {
+    return res.status(400).json({
+      message: "email is already exist",
+    });
+  }
 
-      if(!fullName || !email || !password) {
-        return res.status(400).json({
-          message: "all fields is required"
-        })
-      }
+  const hashedpassword = await bcrypt.hash(password, 10);
 
+  const user = await usermodel.create({
+    fullName,
+    email,
+    password: hashedpassword,
+  });
 
-    const emailisalreadyexist = await usermodel.findOne({
-      email
-    })
+  const token = jwt.sign(
+    {
+      id: user.id,
+    },
+    process.env.SECREATE,
+  );
 
+  res.cookie("token", token);
 
-    if(emailisalreadyexist) {
-      return res.status(400).json({
-        message: "email is already exist"
-      })
-    }
+  res.status(201).json({
+    message: "user registered successfully",
+  });
+}
 
-        const hashedpassword = await bcrypt.hash(password, 10)
-
-        const user = await usermodel.create({
-          fullName,
-          email,
-          password: hashedpassword
-        })
-  
-
- }
-
- module.exports = userregister
+module.exports = userregister;
