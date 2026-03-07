3a2692c801374c06be965a453a6b57e7296bac20
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-03-07 14:20:04 +0500

    new code

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -2,6 +2,12 @@ const foodmodel = require('../models/foodmodel')
 const jwt = require('jsonwebtoken')
 
 
-async function authfoodmiddleeare(req , res , enxt ) {
-  const token = req.cookies
+async function authfoodmiddleeare(req , res , next ) {
+  const token = req.cookies.token;
+
+  if(!token) {
+   return res.status(401).json({
+       message: "plese first login"
+    })
+  }
 }  
