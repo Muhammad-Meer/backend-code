6d80b07cc81f82a1231044d34eeead3117be7dfb
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-03-07 15:17:26 +0500

    new code

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -1,25 +1,24 @@
-const foodmodel = require('../models/foodmodel')
-const jwt = require('jsonwebtoken')
+const foodmodel = require("../models/foodmodel");
+const jwt = require("jsonwebtoken");
 
-
-async function authfoodmiddleeare(req , res , next ) {
+async function authfoodmiddleeare(req, res, next) {
   const token = req.cookies.token;
 
-  if(!token) {
-   return res.status(401).json({
-       message: "plese first login"
-    })
+  if (!token) {
+    return res.status(401).json({
+      message: "plese first login",
+    });
   }
 
   try {
-      const decoded =   jwt.verify(token, process.env.SECREATE)
-      const foodpartner = await foodmodel.findById({
-        
-      })
+    const decoded = jwt.verify(token, process.env.SECREATE);
+    const foodpartner = await foodmodel.findById(decoded.id);
+
+    req.foodpartner = foodpartner;
+    next();
   } catch (error) {
-     
     return res.status(401).json({
-      message: "invalild token "
-    })
+      message: "invalild token ",
+    });
   }
-}  
+}
