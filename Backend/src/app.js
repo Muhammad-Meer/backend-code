500e43b2b4b2555654f63cf55e294c8d3c7ec856
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-03-08 20:53:10 +0500

    fix issue

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const authrouter = require('../src/routes/auth.routes')
+require('dotenv').config();
+
 
 const app = express()
+app.use(express.json())
 
 
-require('dotenv').config();
+
+
 app.use('/api/auth',authrouter)
-
 
 
 
--- Backend/src/controller/auth.controller.js
+++ Backend/src/controller/auth.controller.js
@@ -1,15 +1,30 @@
 const registerusermodel = require("../models/auth.model");
 
 
-const registeruser = (req, res) => {
+const registeruser = async (req, res) => {
   try {
     const { fullneam, email, password } = req.body;
+
 
     if (!fullneam || !email || !password) {
       return res.status(400).json({
         message: "everything is required"
       });
     }
+
+    
+
+    const isemailalreadyexist = await registeruser.findOne({
+      email
+    })
+
+    if(isemailalreadyexist) {
+      return res.status(400).json({
+        message: "user is already exist"
+      })
+    }
+
+    
 
   } catch (error) {
     console.log(error);
