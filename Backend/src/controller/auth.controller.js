857b2fc9666b527ae93024f8ef77e0ee05c8ec23
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-03-09 13:12:37 +0500

    some

--- Backend/src/controller/auth.controller.js
+++ Backend/src/controller/auth.controller.js
@@ -1,53 +1,53 @@
 const registerusermodel = require("../models/auth.model");
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const registeruser = async (req, res) => {
   try {
     const { fullneam, email, password } = req.body;
 
-
     if (!fullneam || !email || !password) {
       return res.status(400).json({
-        message: "everything is required"
+        message: "everything is required",
       });
     }
 
-    
+    const isemailalreadyexist = await registerusermodel.findOne({
+      email,
+    });
 
-    const isemailalreadyexist = await registeruser.findOne({
-      email
-    })
-
-    if(isemailalreadyexist) {
+    if (isemailalreadyexist) {
       return res.status(400).json({
-        message: "user is already exist"
-      })
+        message: "user is already exist",
+      });
     }
 
+    const hashedpassword = await bcrypt.hash(password, 10);
+    console.log(hashedpassword);
 
-
-    const hashedpassword = bcrypt.hash(password, 10)
-    console.log(hashedpassword)
-
-
-
-    const user = await registeruser.create({
+    const user = await registerusermodel.create({
       fullneam,
       email,
-      password: hashedpassword
+      password: hashedpassword,
+    });
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      process.env.SECREATE,
+    );
+
+    res.cookie("token", token);
+
+
+    res.status(201).json({
+      message: "usrer create successfully",
+      user: user
     })
-
-
-  const token = jwt.sign({
-    id: user._id,
-
-  },{})
-
   } catch (error) {
     console.log(error);
   }
 };
 
-module.exports = {registeruser}
+module.exports = { registeruser };
