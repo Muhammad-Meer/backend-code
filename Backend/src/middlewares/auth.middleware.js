b6d604381fe55bdeb465412ea6ac81473f5d8974
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-03-11 14:56:35 +0500

    some issue fix

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -1,13 +1,30 @@
-const foodpartnermodel = require('../models/food.partner.model')
+const foodpartnermodel = require("../models/food.partner.model");
+const jwt = require("jsonwebtoken");
 
-async function authfoodpartnermiddleware(req , res , next) {
-  
-
+async function authfoodpartnermiddleware(req, res, next) {
   const token = req.cookies.token;
 
-  if(!token) {
-     return res.status(401).json({
-     message: "unauthorized access plese logon first"
-    })
+  if (!token) {
+    return res.status(401).json({
+      message: "unauthorized access plese logon first",
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.SECREATE);
+
+  const food = await foodpartnermodel.findOne(decoded.id)
+
+    req.foodpartner = food
+  next()
+
+
+  } catch (error) {
+    return res.status(400).json({
+      messsage: "invalid token",
+    });
   }
 }
+
+
+module.exports = {authfoodpartnermiddleware}
--- Backend/src/routes/foodpartner.routes.js
+++ Backend/src/routes/foodpartner.routes.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 
 
-router.post('/',)
+// router.post('/',)
 
 
 module.exports = router
