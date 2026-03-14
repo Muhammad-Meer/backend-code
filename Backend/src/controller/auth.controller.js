e2dfbac33422fff34514c166baa2d179e4a8d7b7
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-03-14 20:24:18 +0500

    newissue

--- Backend/src/controller/auth.controller.js
+++ Backend/src/controller/auth.controller.js
@@ -57,4 +57,86 @@ const jwt = require('jsonwebtoken');
  }
 
 
-module.exports = {registeruser}
+ async function loginuser(req , res) {
+
+  try {
+    
+  
+   const {  email , password} = req.body
+   console.log("REQ BODY:", req.body)
+
+
+   const user = await usermodel.findOne({
+    email
+   })
+
+   if(!user) {
+    return res.status(400).json({
+      message: "invalid email and password"
+    })
+   }
+
+   const ispasswordvalid = await bcrypt.compare(password, user.password)
+
+   if(!ispasswordvalid) {
+    return res.status(400).json({
+      message: "invalid email and password"
+    })
+   }
+
+   const token = await jwt.sign({
+    id: user._id,
+   },process.env.SECRET, {expiresIn: "6d"})
+
+
+   res.cookie("token", token)
+
+   res.status(200).json({
+    message: "user login successfully",
+    fullname: user.fullname,
+    email: user.email
+   })
+
+     } catch (error) {
+      console.log(error)
+      res.status(500).json({
+        message: "login user some issue"
+      })
+  }
+ }
+
+ function loogoutuser(req , res) {
+    res.clearCookie("token")
+     res.status(200).json({
+      message: "logout successfully"
+    })
+ }
+
+
+ async function registerfoodpartner(params) {
+  try {
+    
+  } catch (error) {
+    
+  }
+ }
+ async function loginfoodpartner(params) {
+  try {
+    
+  } catch (error) {
+    
+  }
+ }
+  function loogoutfoodpartner(req , res) {
+    res.clearCookie("token")
+     res.status(200).json({
+      message: "logout successfully"
+    })
+ }
+ 
+module.exports = {registeruser,
+  loginuser,
+  loogoutuser,
+  registerfoodpartner,
+  loginfoodpartner,
+  loogoutfoodpartner }
--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,11 +1,22 @@
 const express = require('express')
-const router = express()
+const router = express.Router()
 const authcontroller = require('../controller/auth.controller')
 
 
-
+// user api 
 
 router.post('/user/register', authcontroller.registeruser)
+router.post('/user/login', authcontroller.loginuser)
+router.post('/user/loogout', authcontroller.loogoutuser)
+
+
+// foodpartner api 
+
+
+router.post('/user/register', authcontroller.registerfoodpartner)
+router.post('/user/login', authcontroller.loginfoodpartner)
+router.post('/user/loogout', authcontroller.loogoutfoodpartner)
+
+
 
 module.exports = router
-
