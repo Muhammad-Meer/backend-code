88433d8ce389faff797f9e5776f8d91ac937feef
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-03-14 17:11:01 +0500

    issue

--- Backend/src/controller/auth.controller.js
+++ Backend/src/controller/auth.controller.js
@@ -1,2 +1,29 @@
-const mongoose = require('mongoose')
 const usermodel = require('../models/auth.model')
+
+
+  
+ async function registeruser(req , res) {
+
+  try {
+
+  const {fullname ,email , password } = req.body
+
+  if(!fullname || !email  || !password) {
+    return res.status(400).json({
+      message: "all fields are required"
+    })
+  }
+
+  const isUserAlreadyExists = await  usermodel.findOne({
+    email
+  })
+
+  
+
+
+
+} catch (error) {
+  console.log("server error")
+}
+
+}
