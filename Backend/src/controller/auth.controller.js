462bc4437e96ce10db04b780ad94c4ac5ace22d0
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-03-14 17:44:57 +0500

    code

--- Backend/src/controller/auth.controller.js
+++ Backend/src/controller/auth.controller.js
@@ -1,12 +1,12 @@
-const usermodel = require('../models/auth.model')
-
-
+const usermodel = require('../models/auth.model');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
   
  async function registeruser(req , res) {
 
   try {
 
-  const {fullname ,email , password } = req.body
+  const {fullname ,email , password } = req.body;
 
   if(!fullname || !email  || !password) {
     return res.status(400).json({
@@ -18,9 +18,34 @@ const usermodel = require('../models/auth.model')
     email
   })
 
+
+  if(isUserAlreadyExists) {
+    return res.status(400).json({
+      message: "account is alredy exist"
+    })
+  }
+
+  const hashedpassword =  await bcrypt.hash(password, 10)
+
+
+  const user = await usermodel.create({
+   fullname,
+   email,
+   password: hashedpassword
+  })
+
+  const token = jwt.sign({
+    id: user._id,
+  }, process.env.SECRET)
+
+  res.cookie("token" , token)
   
 
-
+  return res.status(201).json({
+    message: "user create successfully",
+    fullname: user.fullname,
+    email: user.email
+  })
 
 } catch (error) {
   console.log("server error")
