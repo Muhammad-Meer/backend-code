c5e0f3da058acc5015c9897dad7943158a571f9e
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-03-17 14:23:51 +0500

    chacha batheja

---
+++ Backend/src/routes/food.routes.js
@@ -0,0 +1,11 @@
+// const mongoose = require('mongoose')
+const express = require('express')
+const foodmodel = require('../models/food.model')
+
+
+
+
+const router = express.Router()
+
+
+module.exports = router
