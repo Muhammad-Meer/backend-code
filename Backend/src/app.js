d8a380d17cad0f19b5118a63ffbf5c04d8078eca
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-03-17 14:52:47 +0500

    code

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,13 +1,15 @@
-const express = require('express')
-require("dotenv").config()
-const cookieParser = require('cookie-parser');
+const express =  require('express')
+const cookieParser = require('cookie-parser')
 const router = require('../src/routes/auth.routes')
+const foodroutes = require('../src/food.routes/')
+require("dotenv").config()
+
 
 const app = express()
-
-app.use(express.json())
 app.use(cookieParser());
+app.use(express.json())
 
 app.use('/api/auth', router)
+app.use('/api/food', foodroutes)
 
-module.exports = app
+export default app
--- Backend/src/routes/food.routes.js
+++ Backend/src/routes/food.routes.js
@@ -1,10 +1,6 @@
-// const mongoose = require('mongoose')
+
+
 const express = require('express')
-const foodmodel = require('../models/food.model')
-
-
-
-
 const router = express.Router()
 
 
