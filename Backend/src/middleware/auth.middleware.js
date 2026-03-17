68c95361891a6d788221bbd7b5a17a1741368615
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-03-17 17:50:11 +0500

    new code

--- Backend/src/middleware/auth.middleware.js
+++ Backend/src/middleware/auth.middleware.js
@@ -2,28 +2,25 @@ const foodpartnermodel = require("../models/food-partner.model");
 const jwt = require("jsonwebtoken");
 
 async function authmiddleware(req, res, next) {
-  const token = req.body;
+  const token = req.headers.authorization?.split(" ")[1]; // Bearer token
 
   if (!token) {
     return res.status(401).json({
-      message: "plese login foodpartner",
+      message: "please login foodpartner",
     });
   }
+
   try {
     const decoded = jwt.verify(token, process.env.SECRET);
-
     const foodpartner = await foodpartnermodel.findById(decoded.id);
-
     req.foodpartner = foodpartner;
-
-     next()
+    next();
   } catch (error) {
     console.log(error);
     res.status(401).json({
-      message: "invalid token ",
+      message: "invalid token",
     });
   }
 }
 
-
-module.exports = {authmiddleware}
+module.exports = authmiddleware;
--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -2,21 +2,14 @@ const express = require('express')
 const router = express.Router()
 const authcontroller = require('../controller/auth.controller')
 
-
 // user api 
-
 router.post('/user/register', authcontroller.registeruser)
 router.post('/user/login', authcontroller.loginuser)
-router.post('/user/loogout', authcontroller.loogoutuser)
-
+router.post('/user/logout', authcontroller.loogoutuser)
 
 // foodpartner api 
-
-
 router.post('/food-partner/register', authcontroller.registerfoodpartner)
 router.post('/food-partner/login', authcontroller.loginfoodpartner)
-router.post('/food-partner/loogout', authcontroller.loogoutfoodpartner)
-
-
+router.post('/food-partner/logout', authcontroller.loogoutfoodpartner)
 
 module.exports = router
